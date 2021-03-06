8721bb3ac4bdb6d1b63faf73ad9470a4fa37bd0b
Kate Arvay <kate@arvay.cz>
2021-03-06 20:04:54 +0100

    init commit

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+
+let video = document.getElementById('bicycle-japan');
+let videoDiv = document.getElementById('video-div');
+let introSong = document.getElementById('intro-song');
+
+window.onload = function() {
+document.getElementById("video-div").addEventListener("mouseenter", function() {
+    document.getElementById("bicycle-japan").play();
+  });
+
+document.getElementById("video-div").addEventListener("mouseleave", function() {
+    document.getElementById("bicycle-japan").pause();
+  });
+
+document.getElementById('video-div').addEventListener("mouseenter", function() {
+  document.getElementById("intro-song").play();
+});
+
+document.getElementById('video-div').addEventListener("mouseleave", function() {
+  document.getElementById("intro-song").pause();
+});
+};
+
