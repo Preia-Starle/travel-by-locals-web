69d975a686bca2578806429618955d372696543c
Kate Arvay <kate@arvay.cz>
2021-03-19 23:04:35 +0100

    local storage clear and redirect button added

---
+++ scripts/table.js
@@ -0,0 +1,7 @@
+let clear = document.getElementById("clear");
+if(clear) clear.addEventListener("click", clearData)
+
+function clearData() {
+    window.localStorage.clear();
+    window.location.replace("./index.html");
+};
