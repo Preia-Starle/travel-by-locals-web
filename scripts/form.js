d41dd3644001ff573d12a62b6b92684223a78106
Kate Arvay <kate@arvay.cz>
2021-03-25 21:40:20 +0100

    input push to table bug fix

--- scripts/form.js
+++ scripts/form.js
@@ -13,7 +13,6 @@ function addData(event) {
         activity_type: document.getElementById("activities").value,
         activity_description: document.getElementById("activity_description").value
     });
-    console.log(arr);
     localStorage.setItem("localData", JSON.stringify(arr));
     console.log("Submitted!");
     document.getElementById("form").reset();
@@ -62,7 +61,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         cell3.innerHTML = arr1[i].profile_picture;
         cell4.innerHTML = arr1[i].interests;
         cell5.innerHTML = arr1[i].about_me;
-        cell6.innerHTML = arr1[i].activities;
+        cell6.innerHTML = arr1[i].activity_type;
         cell7.innerHTML = arr1[i].activity_description;
         cell8.innerHTML = arr1[i].pictures;
     }}
