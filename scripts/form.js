51f4c39822a708c5a03035da194765fbea6bcf98
Kate Arvay <kate@arvay.cz>
2021-04-25 22:23:39 +0200

    location autocomplete added (via google API)

--- scripts/form.js
+++ scripts/form.js
@@ -2,23 +2,38 @@ let click = document.getElementById("submit");
 if (click) click.addEventListener("click", addData);
 
 let arr = new Array();
+let oldArr = new Array();
 
 function addData(event) {
     event.preventDefault();
+    let oldArr = JSON.parse(localStorage.getItem("localData")) || []; 
     arr.push( {
         username: document.getElementById("username").value,
         city: document.getElementById("city").value,
         interests: document.getElementById("interests").value,
         about_me: document.getElementById("about_me").value,
         activity_type: document.getElementById("activities").value,
+        venue: document.getElementById("venue").value,
         activity_description: document.getElementById("activity_description").value
     });
+    if (oldArr.length > 0) {
+        arr.push(oldArr[0]);
+    };
     localStorage.setItem("localData", JSON.stringify(arr));
-    console.log("Submitted!");
     document.getElementById("form").reset();
     successPopUp();
     setTimeout(function() { window.location.replace("./table.html");
-    }, 3000);
+    }, 0);
+    };
+
+let input = document.getElementById('venue');
+if(input) input.addEventListener("click", initVenue);
+
+function initVenue() {
+    let autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.addListener('place_changed', function() {
+        let place = autocomplete.getPlace();
+    });
     };
 
 function successPopUp() {
@@ -37,36 +52,3 @@ function getData(){
 function deleteData() {
     localStorage.clear();
 }
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    let arr1 = new Array();
-    arr1 = JSON.parse(localStorage.getItem("localData"));
-
-    let tbl = document.getElementById("user");
-
-    if (tbl) {
-        for (i = 0; i < arr1.length; i++) {
-        let r = tbl.insertRow();
-        let cell1 = r.insertCell();
-        let cell2 = r.insertCell();
-        let cell3 = r.insertCell();
-        let cell4 = r.insertCell();
-        let cell5 = r.insertCell();
-        let cell6 = r.insertCell();
-        let cell7 = r.insertCell();
-        let cell8 = r.insertCell();
-
-        cell1.innerHTML = arr1[i].username;
-        cell2.innerHTML = arr1[i].city;
-        cell3.innerHTML = arr1[i].profile_picture;
-        cell4.innerHTML = arr1[i].interests;
-        cell5.innerHTML = arr1[i].about_me;
-        cell6.innerHTML = arr1[i].activity_type;
-        cell7.innerHTML = arr1[i].activity_description;
-        cell8.innerHTML = arr1[i].pictures;
-    }}
-
-});
-
-
-
--- scripts/table.js
+++ scripts/table.js
@@ -5,3 +5,42 @@ function clearData() {
     window.localStorage.clear();
     window.location.replace("./index.html");
 };
+
+let addButton = document.getElementById("addPlace");
+if(addButton) addButton.addEventListener("click", formRedirect)
+
+function formRedirect() {
+window.location.replace("./form.html");
+};
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    let arr1 = new Array();
+    arr1 = JSON.parse(localStorage.getItem("localData"));
+
+    let tbl = document.getElementById("user");
+
+    if (tbl) {
+        for (i = 0; i < arr1.length; i++) {
+        let r = tbl.insertRow();
+        let cell1 = r.insertCell();
+        let cell2 = r.insertCell();
+        let cell3 = r.insertCell();
+        let cell4 = r.insertCell();
+        let cell5 = r.insertCell();
+        let cell6 = r.insertCell();
+        let cell7 = r.insertCell();
+        let cell8 = r.insertCell();
+        let cell9 = r.insertCell();
+
+        cell1.innerHTML = arr1[i].username;
+        cell2.innerHTML = arr1[i].city;
+        cell3.innerHTML = arr1[i].profile_picture;
+        cell4.innerHTML = arr1[i].interests;
+        cell5.innerHTML = arr1[i].about_me;
+        cell6.innerHTML = arr1[i].activity_type;
+        cell7.innerHTML = arr1[i].venue;
+        cell8.innerHTML = arr1[i].activity_description;
+        cell9.innerHTML = arr1[i].pictures;
+    }}
+
+});
