65cec76763d60d210ceea70ea8a942157abf2aa6
Kate Arvay <kate@arvay.cz>
2021-05-04 20:17:18 +0200

    form output display styling + js adjustments

--- scripts/table.js
+++ scripts/table.js
@@ -16,31 +16,39 @@ window.location.replace("./form.html");
 window.addEventListener('DOMContentLoaded', (event) => {
     let arr1 = new Array();
     arr1 = JSON.parse(localStorage.getItem("localData"));
-
-    let tbl = document.getElementById("user");
-
-    if (tbl) {
-        for (i = 0; i < arr1.length; i++) {
-        let r = tbl.insertRow();
-        let cell1 = r.insertCell();
-        let cell2 = r.insertCell();
-        let cell3 = r.insertCell();
-        let cell4 = r.insertCell();
-        let cell5 = r.insertCell();
-        let cell6 = r.insertCell();
-        let cell7 = r.insertCell();
-        let cell8 = r.insertCell();
-        let cell9 = r.insertCell();
-
-        cell1.innerHTML = arr1[i].username;
-        cell2.innerHTML = arr1[i].city;
-        cell3.innerHTML = arr1[i].profile_picture;
-        cell4.innerHTML = arr1[i].interests;
-        cell5.innerHTML = arr1[i].about_me;
-        cell6.innerHTML = arr1[i].activity_type;
-        cell7.innerHTML = arr1[i].venue;
-        cell8.innerHTML = arr1[i].activity_description;
-        cell9.innerHTML = arr1[i].pictures;
-    }}
-
-});
+    let results = document.getElementById("results");
+    results.innerHTML = "";
+        for (let i = 0; i < arr1.length; i++) {
+            results.innerHTML += 
+            `<div class="result"> 
+            <table class="table" id="table"> 
+            <tr id="data"> 
+                    <td>Username:</td> 
+                    <td>My City:</td> 
+                    <td>Profile Picture:</td> 
+                    <td>Interests:</td> 
+                    <td>About Me:</td> 
+                    <td>Type Of Activity:</td> 
+                    <td>Venue Name:</td> 
+                    <td>Activity Description:</td>
+                    <td>Pictures:</td> 
+                </tr> 
+                <tr>
+                    <td> ${arr1[i].username} </td> 
+                    <td> ${arr1[i].city} </td> 
+                    <td> ${arr1[i].profile_picture} </td> 
+                    <td> ${arr1[i].interests} </td> 
+                    <td> ${arr1[i].about_me} </td> 
+                    <td> ${arr1[i].activity_type} </td> 
+                    <td> ${arr1[i].venue} </td> 
+                    <td> ${arr1[i].activity_description} </td> 
+                    <td> ${arr1[i].pictures} </td>
+                </tr> 
+            </table> 
+            <div class='map'> 
+            </div> 
+                <span class='material-icons' id='delete'>clear</span> 
+                <span class='material-icons' id='edit'>create</span> 
+        </div>`;
+        };});
+        
