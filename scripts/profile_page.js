da9cc9a414fcdf7a04ce102d75741ea2466715b9
Kate Arvay <kate@arvay.cz>
2021-09-07 20:44:36 +0200

    express api deployment azure

--- scripts/profile_page.js
+++ scripts/profile_page.js
@@ -1,6 +1,6 @@
 //AJAX GET
 let xhr = new XMLHttpRequest();
-xhr.open('GET', '/users', true);
+xhr.open('GET', 'http://localhost:3000/users', true);
 
 xhr.onload = function () {
   let arr1 = JSON.parse("data");
@@ -10,10 +10,10 @@ xhr.onload = function () {
     results.innerHTML +=
     `<div class="result" id="result"> 
             <table class="table" id="table"> 
-            <tr><td class="leftCell">Username:</td><td class="rightCell"> ${arr1username} </td><td id="edit_delete"><span class="material-icons" id="edit" onclick="editItem(${[i]})">create</span><span class="material-icons" id="delete" onclick="deletePopUp(${[i]})">clear</span><span class="material-icons" id="share">share</span></tr></td>
-            <tr><td class="leftCell">My City:</td><td class="rightCell"> ${myCity} </td></tr>
-            <!--<tr><td class="leftCell">Profile Picture:</td><td class="rightCell"> ${profile_picture} </td></tr>--> 
-            <tr><td class="leftCell">Interests:</td><td class="rightCell"> ${interests} </td></tr> 
+            <tr><td class="leftCell">Username:</td><td class="rightCell"> ${arr1[i].username} </td><td id="edit_delete"><span class="material-icons" id="edit" onclick="editItem(${[i]})">create</span><span class="material-icons" id="delete" onclick="deletePopUp(${[i]})">clear</span><span class="material-icons" id="share">share</span></tr></td>
+            <tr><td class="leftCell">My City:</td><td class="rightCell"> ${arr1[i].myCity} </td></tr>
+            <!--<tr><td class="leftCell">Profile Picture:</td><td class="rightCell"> ${arr1[i].profile_picture} </td></tr>--> 
+            <tr><td class="leftCell">Interests:</td><td class="rightCell"> ${arr1[i].interests} </td></tr> 
             <tr><td class="leftCell">About Me:</td><td  class="rightCell"style="text-overflow: ellipsis; white-space: normal; word-break: break-all"> ${arr1[i].about_me} </td></tr> 
             </table>`
   }
