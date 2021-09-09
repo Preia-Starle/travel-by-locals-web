ca203c058716fce63a2be34d43effdc934fea1d5
Kate Arvay <kate@arvay.cz>
2021-09-10 00:32:36 +0200

    adjust api endpoint url after deployment

--- TravelsByLocals API/node_express_api/index.js
+++ TravelsByLocals API/node_express_api/index.js
@@ -23,5 +23,5 @@ app.use(cors({
 
 app.use('/users', usersRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port: https://kate.arvay.cz:${PORT}`));
 
